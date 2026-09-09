0c3a4650ed9419441c113abfd7a77df7c73515f4
mateuscmp <mcmp1712@gmail.com>
2026-09-09 16:54:09 -0300

    + algumas coisa

--- apresentacao/interface.py
+++ apresentacao/interface.py
@@ -12,9 +12,9 @@ def telaLogin() -> Usuario:
 
 def criarUsuario() -> Usuario:
     while True:
-        usuario: str = input("Insira seu nome de usuário ou digite \'sair\' para sair: ")
+        usuario: str = input("Digite um nome de usuário ou digite \'sair\' para sair: ")
         if usuario == 'sair': break
-        senha: str = input("Digite sua senha: ")
+        senha: str = input("Crie sua senha: ")
         usuarioCriado: tuple[bool,Usuario] = sessao_login.criarUsuario(usuario, senha)
         if usuarioCriado[0]:
             return usuarioCriado[1]
